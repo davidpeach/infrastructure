5ab104a26d4913156327b180afb1fba530be0e37
David Peach <hello@davidpeach.me>
2022-11-18 11:54:46 +0000

    Add spaces. Add variables file. Some tidying.

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "davidpeach"
+
+    workspaces {
+      name = "redemption"
+    }
+  }
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
@@ -15,6 +22,13 @@ terraform {
 # or using -var="do_token=..." CLI option
 variable "do_token" {}
 variable "github_token" {}
+variable "fully_qualified_domain" {}
+variable "registry_name" {}
+variable "database_cluster_name" {}
+variable "database_name" {}
+variable "kubernetes_cluster_name" {}
+variable "node_pool_name" {}
+variable "spaces_bucket_name" {}
 
 provider "digitalocean" {
   token = var.do_token
@@ -24,49 +38,49 @@ provider "github" {
     token = var.github_token
 }
 
-resource "digitalocean_vpc" "davidpeach-me-network" {
-  name   = "davidpeach.me"
+resource "digitalocean_vpc" "vpc_network" {
+  name   = var.fully_qualified_domain
   region = "lon1"
 }
 
-resource "digitalocean_container_registry" "newhanover-registry" {
-  name                   = "rhodes"
+resource "digitalocean_container_registry" "registry" {
+  name                   = var.registry_name
   subscription_tier_slug = "starter"
   region                  = "ams3"
 }
 
-resource "digitalocean_database_db" "newhanover-mysql-davidpeach-me" {
-  cluster_id = digitalocean_database_cluster.newhanover-mysql-cluster.id
-  name       = "davidpeachme"
+resource "digitalocean_database_db" "database" {
+  cluster_id = digitalocean_database_cluster.database_cluster.id
+  name       = var.database_name
 }
 
-resource "digitalocean_database_cluster" "newhanover-mysql-cluster" {
-  name       = "newhanover-mysql-cluster"
+resource "digitalocean_database_cluster" "database_cluster" {
+  name       = var.database_cluster_name
   engine     = "mysql"
   version    = "8"
   size       = "db-s-1vcpu-1gb"
   region     = "lon1"
   node_count = 1
-  private_network_uuid = digitalocean_vpc.davidpeach-me-network.id
+  private_network_uuid = digitalocean_vpc.vpc_network.id
 }
 
-resource "digitalocean_kubernetes_cluster" "newhanover-k8s-cluster" {
-  name   = "newhanover"
+resource "digitalocean_kubernetes_cluster" "kubernetes_cluster" {
+  name   = var.kubernetes_cluster_name
   region = "lon1"
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.24.4-do.0"
 
   node_pool {
-    name       = "valentine"
+    name       = var.node_pool_name
     size       = "s-1vcpu-2gb"
-    node_count = 3
+    node_count = 2
   }
 }
 
 resource "github_actions_secret" "registry_endpoint_secret" {
   repository       = "laravel-app-k8s"
   secret_name      = "DO_REGISTRY_ENDPOINT"
-  plaintext_value  = digitalocean_container_registry.newhanover-registry.endpoint
+  plaintext_value  = digitalocean_container_registry.registry.endpoint
 }
 
 # Create a github action secret for repos for the DO Token.
@@ -76,21 +90,27 @@ resource "github_actions_secret" "do_access_token_secret" {
   plaintext_value  = var.do_token
 }
 
-/* resource "digitalocean_spaces_bucket" "peach-spaces-bucket" { */
-/*   name   = "peach-spaces-bucket" */
-/*   region = "ams3" */
+resource "github_actions_secret" "laravel_env_encryption_key_secret" {
+  repository       = "laravel-app-k8s"
+  secret_name      = "LARAVEL_ENV_ENCRYPTION_KEY"
+  plaintext_value  = "3UVsEgGVK36XN82KKeyLFMhvosbZN1aF"
+}
 
-/*   cors_rule { */
-/*     allowed_headers = ["*"] */
-/*     allowed_methods = ["GET"] */
-/*     allowed_origins = ["*"] */
-/*     max_age_seconds = 3000 */
-/*   } */
+resource "digitalocean_spaces_bucket" "bucket" {
+  name   = var.spaces_bucket_name
+  region = "ams3"
 
-/*   cors_rule { */
-/*     allowed_headers = ["*"] */
-/*     allowed_methods = ["PUT", "POST", "DELETE"] */
-/*     allowed_origins = ["https://davidpeach.me"] */
-/*     max_age_seconds = 3000 */
-/*   } */
-/* } */
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "DELETE"]
+    allowed_origins = [var.fully_qualified_domain]
+    max_age_seconds = 3000
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+fully_qualified_domain = "https://davidpeach.co.uk"
+registry_name = "rhodes"
+database_cluster_name = "west-elizabeth"
+database_name = "strawberry"
+kubernetes_cluster_name = "new-hanover"
+node_pool_name = "valentine"
+spaces_bucket_name = "blackwater"
